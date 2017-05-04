a6ff1bebeaf531f8ef1becec129703300344344f
sokama <kovacs.beno@freemail.hu>
2017-05-04 22:51:24 +0200

    Added CubeTest

--- WaxUnitTest/UnitTest1.cs
+++ WaxUnitTest/UnitTest1.cs
@@ -11,11 +11,21 @@ namespace WaxUnitTest
         static Expression<Func<int, int>> Square = x => x * x;
         static Expression<Func<int, int>> SquSquare = Wax.Unwrap<int, int>(
         x => Square.Expand(Square.Expand(x)));
+        static Expression<Func<int, int>> Cube = Wax.Unwrap<int, int>(
+            x => SquSquare.Expand(x) / x);
+
         [TestMethod]
-        public void TestMethod1()
+        public void SquSquareTest()
         {
-            Expression<Func<int,int>> myexp = x=>  x*x;
-            Assert.AreEqual(Square.Body.ToString(), myexp.Body.ToString());
+            Expression<Func<int,int>> myexp = x=>  (x*x)*(x*x);
+            Assert.AreEqual(SquSquare.Body.ToString(), myexp.Body.ToString());
+        }
+
+        [TestMethod]
+        public void CubeTest()
+        {
+            Expression<Func<int, int>> myexp = x => (x * x) * (x * x) / x;
+            Assert.AreEqual(Cube.Body.ToString(), myexp.Body.ToString());
         }
     }
     
